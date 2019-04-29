dfc2b3d7b8176d2e045b134eea238f8efae5571a
Alessandro Riega Estrada <alessandro.riega@gmail.com>
2019-04-28 19:19:03 -0500

    removing comments

--- src/main/java/com/home/springboot/app/models/service/CustomerServiceImpl.java
+++ src/main/java/com/home/springboot/app/models/service/CustomerServiceImpl.java
@@ -52,13 +52,11 @@ public class CustomerServiceImpl implements ICustomerService {
 	@Transactional(readOnly = true)
 	@Override
 	public List<Customer> getProcedureCursorById(Long p_cursor_id) throws SQLException {
-		System.out.println("IĐĐĐĐĐĐĐĐĐĐĐĐĐĐĐĐ " + p_cursor_id);
 		List<ResultSet> rscustomers = custRepo.fetchCustomerById(p_cursor_id);
-
 		List<Customer> customers = new ArrayList<Customer>();
+
 		for (int i = 0; i < rscustomers.size(); i++) {
 			ResultSet rs = rscustomers.get(i);
-			System.out.println("antes del whil");
 			while (rs.next()) {
 				Customer customer = new Customer();
 				customer.setName(rs.getString("name"));
@@ -69,26 +67,5 @@ public class CustomerServiceImpl implements ICustomerService {
 		}
 		return customers;
 	}
-
-//	@Transactional
-//	@Override
-//	public List<Customer> getProcedureCursor() throws SQLException {
-//		List<ResultSet> rscustomers = custRepo.fetchCustomer();
-//
-//		List<Customer> customers = new ArrayList<Customer>();
-//
-//		for (int i = 0; i < rscustomers.size(); i++) {
-//			ResultSet rs = rscustomers.get(i);
-//			while (rs.next()) {
-//				Customer customer = new Customer();
-//				customer.setId(rs.getLong("id"));
-//				customer.setName(rs.getString("name"));
-//				customer.setEmail(rs.getString("email"));
-//				customer.setCreatedDate(rs.getDate("created_date"));
-//				customers.add(customer);
-//			}
-//		}
-//		return customers;
-//	}
 
 }
